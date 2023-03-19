ae167926fc93f9b6dfc23187483d9cecd8ad6f9f
Braedon Lewis <braedonwilcox@gmail.com>
2023-03-19 14:04:50 -0400

    Remove raylib-api folder

--- raylib-api/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    raylib: require('./raylib.json'),
-    raymath: require('./raymath.json'),
-    rlgl: require('./rlgl.json'),
-    rmem: require('./rmem.json'),
-    reasings: require('./reasings.json'),
-    raygui: require('./raygui.json'),
-    rres: require('./rres.json')
-}
